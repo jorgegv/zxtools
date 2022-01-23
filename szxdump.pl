bdcc1e4d89b10a90d2b165d52674d22e607569a0
Jorge Gonzalez <jorge.gonzalez@daikon.es>
2022-01-23 11:32:20 +0100

    szxdump: added base address to memory bank dump

--- szxdump.pl
+++ szxdump.pl
@@ -108,18 +108,23 @@ sub decode_szx_block {
 ## Main
 ##
 
-our( $opt_f, $opt_b );
-getopts( 'b:f:' );
+our( $opt_f, $opt_b, $opt_a );
+getopts( 'b:f:a:' );
 
 defined( $opt_f ) or
     die "
-usage: $0 <file.szx> [-b <n>]
+usage: $0 <file.szx> [-b <n>] [-a <address>]
   The SZX file is mandatory. If no -b argument is used, a summary of the SZX file structure is output.
   If -b <n> is supplied, an hex dump of memory bank number <n> is output.
+  If -b and -a are supplied, the memory dump will be based on address <address> instead of 0x0000
 ";
 
 my $szx_file = $opt_f;
 my $bank_to_dump = $opt_b;
+my $memory_dump_base_address = ( defined( $opt_a ) ? $opt_a : 0 );
+if ( $memory_dump_base_address =~ /^0[xX]([\dA-Fa-f]+)/ ) {
+    $memory_dump_base_address = hex( $1 );
+}
 
 open( my $fh, "<", $szx_file ) or
     die "Could not open $szx_file for reading: $!\n";
@@ -150,18 +155,21 @@ while ( not eof $fh ) {
 
             my @dump_lines;
             my @dump_bytes = unpack( 'C*', $block->{'data'}{'uncompressed_data'} );
-            my $current_line;
+            my $current_address = $memory_dump_base_address;
+            my $current_line = sprintf( '%06X: ', $current_address );
             my $cnt = 1;
             foreach my $byte ( @dump_bytes ) {
                 $current_line .= sprintf( '%02X', $byte ) . ' ';
                 if ( ( $cnt++ % 16 ) == 0 ) {
                     push @dump_lines, $current_line;
-                    $current_line = '';
+                    $current_address += 16;
+                    $current_line = sprintf( '%06X: ', $current_address );
                 }
             }
 
             print  "   Memory dump:\n      ";
             print join( "\n      ", @dump_lines );
+            print "\n";
         }
     }
 }
