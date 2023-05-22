70296896bc8cbc63230f7ae224bd1967b87440f1
ZXjogv <zx@jogv.es>
2023-05-23 00:20:52 +0200

    spritegen: initial doc and skeleton (WIP)

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Tools for ZX Spectrum development
 ## Tools
 
 * **szxdump.pl:** decodes SZX files including memory contents
+* **spritegen.pl:** generates ASM and C sprite definitions from PNG files
 
 ## Requirements
 
---
+++ spritegen.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+-i <png_file>
+-m <mask_color>
+-f <foreground_color>
+-b <background_color>
+-x <x_position>
+-y <y_position>
+-w <width>
+-h <height>
+-c <c|asm>
+-n <C/ASM identifier>
+-l <scanlines,rows,columns>
+-t <load,mask>
+-r (generate empty extra right column)
+-q (generate empty extra bottom row)
