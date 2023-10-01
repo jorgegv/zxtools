ebbe1ce5ff48340c33ca008048dec4a7a4dd5c8f
ZXjogv <zx@jogv.es>
2023-10-01 12:24:19 +0200

    szxdump: change -f for -i parameter specification

--- bin/szxdump.pl
+++ bin/szxdump.pl
@@ -66,18 +66,18 @@ my $block_summary_function = {
 ## Main
 ##
 
-our( $opt_f, $opt_b, $opt_a );
-getopts( 'b:f:a:' );
+our( $opt_i, $opt_b, $opt_a );
+getopts( 'b:i:a:' );
 
-defined( $opt_f ) or
+defined( $opt_i ) or
     die "
-usage: $0 -f <file.szx> [-b <n>] [-a <address>]
+usage: $0 -i <file.szx> [-b <n>] [-a <address>]
   The SZX file is mandatory. If no -b argument is used, a summary of the SZX file structure is output.
   If -b <n> is supplied, an hex dump of memory bank number <n> is output.
   If -b and -a are supplied, the memory dump will be based on address <address> instead of 0x0000
 ";
 
-my $szx_file = $opt_f;
+my $szx_file = $opt_i;
 my $bank_to_dump = $opt_b;
 my $memory_dump_base_address = ( defined( $opt_a ) ? $opt_a : 0 );
 if ( $memory_dump_base_address =~ /^0[xX]([\dA-Fa-f]+)/ ) {
